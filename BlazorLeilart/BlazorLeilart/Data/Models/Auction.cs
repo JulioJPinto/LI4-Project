c50c365280bc6ea629ba7823433b8e173fb2b184
Júlio Pinto <pinto.julioj03@gmail.com>
2024-01-22 14:17:44 +0000

    fixed auctions page connection

--- BlazorLeilart/BlazorLeilart/Data/Models/Auction.cs
+++ BlazorLeilart/BlazorLeilart/Data/Models/Auction.cs
@@ -6,46 +6,47 @@ using System;
 
 public class Auction
 {
-    public int Id { get; private set; }
-    public int TipoLeilao { get; private set; }
-    public int IdProduto { get; private set; }
-    public int? ValorMinimo { get; private set; }
-    public int? ValorInicial { get; private set; }
-    public int? Incremento { get; private set; }
-    public DateTime Inicio { get; private set; }
-    public DateTime Fim { get; private set; }
-    public int? LicitacaoAtual { get; private set; }
-    public bool Estado { get; private set; }
+    public int id { get; private set; }
+    public int auction_type_id { get; private set; }
+    public int product_id { get; private set; }
+    public int? minimum_value { get; private set; }
+    public int? initial_value { get; private set; }
+    public int? increment { get; private set; }
+    public DateTime start { get; private set; }
+    public DateTime end { get; private set; }
+    public int? current_bid { get; private set; }
+    public bool status { get; private set; }
     
     public Product Product { get; set; }
 
-    public Auction(int id, int tipo_leilao, int produto, int valor_minimo, int valor_inicial, int incremento, DateTime inicio, DateTime fim, int licitacao_atual, bool estado)
+    public Auction(int id, int auctionTypeId, int productId, int? minimumValue, int? initialValue, int? increment, DateTime start, DateTime end, int? currentBid, bool status)
     {
-        this.Id = id;
-        this.TipoLeilao = tipo_leilao;
-        this.IdProduto = produto;
-        this.ValorMinimo = valor_minimo;
-        this.ValorInicial = valor_inicial;
-        this.Incremento = incremento;
-        this.Inicio = inicio;
-        this.Fim = fim;
-        this.LicitacaoAtual = licitacao_atual;
-        this.Estado = estado;
+        this.id = id;
+        this.auction_type_id = auctionTypeId;
+        this.product_id = productId;
+        this.minimum_value = minimumValue;
+        this.initial_value = initialValue;
+        this.increment = increment;
+        this.start = start;
+        this.end = end;
+        this.current_bid = currentBid;
+        this.status = status;
         this.Product = null;
     }
 
+
     public Auction()
     {
-        this.Id = -1;
-        this.TipoLeilao = -1;
-        this.IdProduto = -1;
-        this.ValorMinimo = -1;
-        this.ValorInicial = -1;
-        this.Incremento = -1;
-        this.Inicio = System.DateTime.Now;
-        this.Fim = System.DateTime.Now;
-        this.LicitacaoAtual = -1;
-        this.Estado = false;
+        this.id = -1;
+        this.auction_type_id = -1;
+        this.product_id = -1;
+        this.minimum_value = -1;
+        this.initial_value = -1;
+        this.increment = -1;
+        this.start = System.DateTime.Now;
+        this.end = System.DateTime.Now;
+        this.current_bid = -1;
+        this.status = false;
         this.Product = null;
     }
     
